bf7615f4d169a008298d3baffd68a13f25b90bf0
rockinbj <luxxsys@gmail.com>
2021-10-09 22:09:31 +0800

    new hawkassit invest btc periodically

---
+++ index.js
@@ -0,0 +1,35 @@
+const shell = require("shelljs");
+const {getAhr999, getAhr999x} = require("./ahr999");
+
+
+let buyAmount = 0.001;
+let interval = 5;  // minute
+let usdtCny = 6.55;
+
+
+async function main() {
+    let ahr999 = await getAhr999();
+    console.log(`ahr999: ${ahr999}`);
+    if (ahr999 <= 0.46) {
+        buyAmount = (13.88/usdtCny).toFixed(4);
+        interval = 15;
+        
+    } 
+    else if (ahr999 > 0.46 && ahr999 <= 1.2) {
+        buyAmount = (13.88/usdtCny).toFixed(4);
+        interval = 30;
+    }
+    else {
+        buyAmount = (6.49/usdtCny).toFixed(4);
+        interval = 60;
+    }
+
+    cmd = `*/${interval} * * * * cd /home/rockhawk/Code/mixin/hawkassist; node mixswap.js ${buyAmount} > mixswap.log &`
+    // console.log(cmd);
+    shell.exec(`echo "${cmd}" > cron.mixswap`);
+    shell.exec("crontab cron.mixswap");
+    shell.exec("crontab -l");
+}
+
+
+main();
---
+++ mixswap.js
@@ -0,0 +1,107 @@
+// Usage: buying btc in mixswap with 100.123 usdt then transfer back to rock21834
+//   node mixswap.js 100.123
+// it will check balance automatically.
+
+const axios = require("axios");
+const {BlazeClient} = require("mixin-node-sdk");
+const mixinAppConfig = require("./mixinConfig");
+
+const client = new BlazeClient(mixinAppConfig, {parse: true, syncAck: true})
+
+
+const myUserId = "b7366a96-afd9-4b1c-bb74-9084196959d3";  //Rock 21834
+const mixswapApiPoint = "https://mixswap.exchange/api/v1";
+const mixswapUserId = "6a4a121d-9673-4a7e-a93e-cb9ca4bb83a2";
+const assetIdBtc = "c6d0c728-2624-429b-8e0d-d9d19b6592fa";
+// const assetIdBtc = "965e5c6e-434c-3fa9-b780-c50f43cd955c";  //cnb for test
+
+const assetIdUsdt = "4d8c508b-91c5-375b-92b0-ee702ed2dac5";
+const buyAmount = process.argv[2];  // get amount argument from cmd line
+const buyMemo = Buffer.from(`0|${assetIdBtc}`).toString("base64");
+const buyTraceId = client.newUUID();
+const waitTime = 3000; // ms
+
+
+// await sleep(ms)
+function sleep(ms) {
+    return new Promise(resolve=>{
+        setTimeout(resolve, ms);
+    })
+}
+
+
+async function checkBalance(assetId, amount) {
+    let balance = await client.readAsset(assetId);
+    balance = balance.balance;
+    console.log(`${assetId} balance ${balance}`);
+    if (balance >= amount) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+async function getMixswapOrderStatus(traceId) {
+    orderStatus = "";
+    let r = "";
+    for (let i = 0; i < 15; i++) {
+        await sleep(waitTime);
+        // console.log(`${mixswapApiPoint}/order/${traceId}`);
+        try {
+            r = await axios.get(`${mixswapApiPoint}/order/${traceId}`);
+            r = r.data;
+        } catch (error) {
+            console.log(error);
+        }
+
+        if (r.success === true) {
+            orderStatus = r.data.orderStatus;
+            // console.log(orderStatus);
+        }
+        
+        if (orderStatus == "done") {
+            console.log(r);
+            return r.data.receiveAmount;
+        }
+    }
+    return false;
+}
+
+
+const buyTransInfo = {
+    asset_id: assetIdUsdt,
+    opponent_id: mixswapUserId,
+    amount: buyAmount,
+    trace_id: buyTraceId,
+    memo: buyMemo,
+}
+
+
+async function main() {
+    if (await checkBalance(assetIdUsdt, buyAmount)) {
+        // transfer usdt to mixswap for buying btc
+        client.transfer(buyTransInfo).then(async r=>{
+            console.log(r);
+            receiveAmount = await getMixswapOrderStatus(r.trace_id);
+            if (receiveAmount) {
+                const transBackMemo = `${buyAmount}USDT->${receiveAmount}BTC with Mixswap`
+                backTransInfo = {
+                    asset_id: assetIdBtc,
+                    opponent_id: myUserId,
+                    amount: receiveAmount,
+                    trace_id: client.newUUID(),
+                    memo: transBackMemo,
+                }
+                console.log(backTransInfo);
+                client.transfer(backTransInfo).then(console.log)
+            }
+        });
+    }
+    else{
+        client.sendTextMsg(myUserId, "️️️️️⚠️Insufficient balance, please deposit USDT.");
+    }
+}
+
+
+main();
